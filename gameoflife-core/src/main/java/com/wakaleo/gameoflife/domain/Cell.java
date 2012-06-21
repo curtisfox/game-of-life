050865af3bff86f250c23bce557fd1709ba76b4f
Curtis Fox <curtis.fox@wearemanifesto.co.uk>
2012-06-21 10:39:15 +0100

    Changes stars to pluses

--- gameoflife-core/src/main/java/com/wakaleo/gameoflife/domain/Cell.java
+++ gameoflife-core/src/main/java/com/wakaleo/gameoflife/domain/Cell.java
@@ -3,7 +3,7 @@ package com.wakaleo.gameoflife.domain;
 public enum Cell {
     LIVE_CELL("+"), DEAD_CELL(".");
 
-    private String symbolfail;
+    private String symbol;
 
     private Cell(final String initialSymbol) {
         this.symbol = initialSymbol;
