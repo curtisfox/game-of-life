f69b8298056e0d2af10c06d547835b0a27ab6f78
Curtis Fox <curtis.fox@wearemanifesto.co.uk>
2012-06-21 10:35:09 +0100

    Changed symbol to fail

--- gameoflife-core/src/main/java/com/wakaleo/gameoflife/domain/Cell.java
+++ gameoflife-core/src/main/java/com/wakaleo/gameoflife/domain/Cell.java
@@ -3,7 +3,7 @@ package com.wakaleo.gameoflife.domain;
 public enum Cell {
     LIVE_CELL("+"), DEAD_CELL(".");
 
-    private String symbol;
+    private String symbolfail;
 
     private Cell(final String initialSymbol) {
         this.symbol = initialSymbol;
